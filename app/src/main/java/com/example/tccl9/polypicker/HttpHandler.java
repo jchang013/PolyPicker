cc082245517e6753c9bbae2bd29ec22305cdd929
jchang013 <44076844+jchang013@users.noreply.github.com>
2018-10-31 15:14:07 +0800

    Added HttpHandler class for calling data.gov.sg api

---
+++ app/src/main/java/com/example/tccl9/polypicker/HttpHandler.java
@@ -0,0 +1,17 @@
+package com.example.tccl9.polypicker;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class HttpHandler {
+
+}
