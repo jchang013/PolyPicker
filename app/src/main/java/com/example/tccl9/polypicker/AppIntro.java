81465931bd73494d4d4fef0254d25574ac31bd6b
jchang013 <44076844+jchang013@users.noreply.github.com>
2018-11-03 20:40:46 +0800

    Successfully added RP, NYP and TP into database. All polytechnic are in phone database now.

--- app/src/main/java/com/example/tccl9/polypicker/AppIntro.java
+++ app/src/main/java/com/example/tccl9/polypicker/AppIntro.java
@@ -47,6 +47,15 @@ public class AppIntro extends AppCompatActivity {
             if (courseDatabase.polytechnicsInDatabase().contains("Ngee Ann Polytechnic")) {
                 npInDatabase = true;
             }
+            if (courseDatabase.polytechnicsInDatabase().contains("Republic Polytechnic")) {
+                rpInDatabase = true;
+            }
+            if (courseDatabase.polytechnicsInDatabase().contains("Nanyang Polytechnic")) {
+                nypInDatabase = true;
+            }
+            if (courseDatabase.polytechnicsInDatabase().contains("Temasek Polytechnic")) {
+                tpInDatabase = true;
+            }
             new getCourseData().execute();
         }
 
@@ -67,7 +76,7 @@ public class AppIntro extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Toast.makeText(AppIntro.this, "Course Data is downloading", Toast.LENGTH_LONG).show();
+            //Toast.makeText(AppIntro.this, "Course data is downloading", Toast.LENGTH_LONG).show();
 
         }
 
@@ -79,6 +88,15 @@ public class AppIntro extends AppCompatActivity {
             }
             if (npInDatabase == false) {
                 getDataAPIRequest(npDataAPI, "Ngee Ann Polytechnic");
+            }
+            if (rpInDatabase == false) {
+                getDataAPIRequestRP(rpDataAPI, "Republic Polytechnic");
+            }
+            if (nypInDatabase == false) {
+                getDataAPIRequestNYP(nypDataAPI, "Nanyang Polytechnic");
+            }
+            if (tpInDatabase == false) {
+                getDataAPIRequestTP(tpDataAPI, "Temasek Polytechnic");
             }
             return null;
         }
@@ -101,7 +119,150 @@ public class AppIntro extends AppCompatActivity {
                         String description = course.getString("course_description");
                         int cutoff = 0;         //add in method to return cutoff point with course code later
                         String link = course.getString("reference");
-                        //int bookmark = 0;       //bookmark set as false initially
+                        courseDatabase.insertData(code, name, school, polytechnic, description, cutoff, link);
+                    }
+
+                } catch (final JSONException e) {
+                    //Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+            else {
+                //Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from data.gov.sg.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+        protected void getDataAPIRequestRP(String url, String polytechnic) {
+            //For RP, NYP and TP data API, the columns are different and some columns not available
+            //This is for RP
+            HttpHandler hh = new HttpHandler();
+            String jsonStr = hh.makeServiceCall(url);
+
+            //Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONObject jsonResult = jsonObj.getJSONObject("result");
+                    JSONArray courses = jsonResult.getJSONArray("records");
+
+                    for (int i = 0; i < courses.length(); i++) {
+                        JSONObject course = courses.getJSONObject(i);
+                        String code = course.getString("course_code");
+                        String name = course.getString("course_name");
+                        String school = course.getString("school");
+                        //String description = course.getString("course_description");
+                        int cutoff = 0;         //add in method to return cutoff point with course code later
+                        //String link = course.getString("reference");
+                        courseDatabase.insertData(code, name, school, polytechnic, null, cutoff, null);
+                    }
+
+                } catch (final JSONException e) {
+                    //Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+            else {
+                //Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from data.gov.sg.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+        protected void getDataAPIRequestNYP(String url, String polytechnic) {
+            //For RP, NYP and TP data API, the columns are different and some columns not available
+            //This is for NYP
+            HttpHandler hh = new HttpHandler();
+            String jsonStr = hh.makeServiceCall(url);
+
+            //Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONObject jsonResult = jsonObj.getJSONObject("result");
+                    JSONArray courses = jsonResult.getJSONArray("records");
+
+                    for (int i = 0; i < courses.length(); i++) {
+                        JSONObject course = courses.getJSONObject(i);
+                        String code = course.getString("jae_course_code");
+                        String name = course.getString("course_title");
+                        //String school = course.getString("school");
+                        //String description = course.getString("course_description");
+                        int cutoff = 0;         //add in method to return cutoff point with course code later
+                        String link = course.getString("url");
+                        courseDatabase.insertData(code, name, null, polytechnic, null, cutoff, link);
+                    }
+
+                } catch (final JSONException e) {
+                    //Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+            else {
+                //Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from data.gov.sg.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+        protected void getDataAPIRequestTP(String url, String polytechnic) {
+            //For RP, NYP and TP data API, the columns are different and some columns not available
+            //This is for TP
+            HttpHandler hh = new HttpHandler();
+            String jsonStr = hh.makeServiceCall(url);
+
+            //Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONObject jsonResult = jsonObj.getJSONObject("result");
+                    JSONArray courses = jsonResult.getJSONArray("records");
+
+                    for (int i = 0; i < courses.length(); i++) {
+                        JSONObject course = courses.getJSONObject(i);
+                        String code = course.getString("moe_course_code");
+                        String name = course.getString("course_name");
+                        String school = course.getString("school");
+                        String description = course.getString("course_description");
+                        int cutoff = 0;         //add in method to return cutoff point with course code later
+                        String link = course.getString("reference");
                         courseDatabase.insertData(code, name, school, polytechnic, description, cutoff, link);
                     }
 
